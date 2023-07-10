4172a3c996b2d123ccdaecae2bd835f747562fa0
Andrew Lima <andrewmll94@gmail.com>
2023-07-10 15:37:34 -0300

    Feat: Implementação da classe abstrata PessoaCandidata, bem como seus métodos e herdando da classe Pessoa, ficando responsável por representar a pessoa candidata.

--- src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
+++ src/main/java/com/betrybe/sistemadevotacao/PessoaCandidata.java
@@ -1,5 +1,46 @@
 package com.betrybe.sistemadevotacao;
 
-public class PessoaCandidata {
+public class PessoaCandidata extends Pessoa {
+  private int numero;
+  private int votos;
 
+  /**
+  * Método construtor.
+  */
+  
+  public PessoaCandidata(String nome, int numero) {
+    super.nome = nome;
+    this.numero = numero;
+    this.votos = 0;
+  }
+
+  @Override
+  public String getNome() {
+    return super.getNome();
+  }
+
+  @Override
+  public void setNome(String nome) {
+    super.setNome(nome);
+  }
+
+  public int getNumero() {
+    return this.numero;
+  }
+
+  public void setNumero(int numero) {
+    this.numero = numero;
+  }
+
+  public int getVotos() {
+    return this.votos;
+  }
+
+  public void setVotos(int votos) {
+    this.votos = votos;
+  }
+
+  public void receberVoto() {
+    this.votos += 1;
+  }
 }
